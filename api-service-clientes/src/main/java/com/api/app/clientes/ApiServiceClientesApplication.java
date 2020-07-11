749bbb105e3c23f3ea2ecedb3d5582b84e813c28
Niko E. Rozo E <niko.rozo92@outlook.com>
2020-07-11 17:32:29 -0500

    Crear Proyecto Api Cliente

---
+++ api-service-clientes/src/main/java/com/api/app/clientes/ApiServiceClientesApplication.java
@@ -0,0 +1,13 @@
+package com.api.app.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceClientesApplication.class, args);
+	}
+
+}
---
+++ api-service-clientes/src/test/java/com/api/app/clientes/ApiServiceClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.app.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
