00c2a712e179cec46b512649c5e06a9f64464b80
Niko E. Rozo E <niko.rozo92@outlook.com>
2020-07-16 09:05:30 -0500

    Se Quita Cors, Conflicto con el Proxy Reverso

--- api-service-clientes/src/main/java/com/api/app/clientes/controllers/ClienteRestController.java
+++ api-service-clientes/src/main/java/com/api/app/clientes/controllers/ClienteRestController.java
@@ -3,14 +3,12 @@ package com.api.app.clientes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.app.clientes.model.entity.Cliente;
 import com.api.app.clientes.model.services.IClienteService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class ClienteRestController {
 
