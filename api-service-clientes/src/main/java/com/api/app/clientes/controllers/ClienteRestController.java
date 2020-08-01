df7ef6b8e19ed30d046d324d80cab1b1fdd3706c
Niko E. Rozo E <niko.rozo92@outlook.com>
2020-08-01 13:25:08 -0500

    Cargue y consulta de Imagen del perfil

--- api-service-clientes/src/main/java/com/api/app/clientes/controllers/ClienteRestController.java
+++ api-service-clientes/src/main/java/com/api/app/clientes/controllers/ClienteRestController.java
@@ -1,28 +1,40 @@
 package com.api.app.clientes.controllers;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.api.app.clientes.model.entity.Cliente;
 import com.api.app.clientes.model.services.IClienteService;
@@ -32,6 +44,8 @@ public class ClienteRestController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	private final Logger log = LoggerFactory.getLogger(ClienteRestController.class);
 	
 	@GetMapping("/")
 	public List<Cliente> findAll(){
@@ -131,6 +145,10 @@ public class ClienteRestController {
 	public ResponseEntity<?> delete(@PathVariable Long id){
 		Map<String, Object> response = new HashMap<>();
 		try {
+			Cliente cliente = clienteService.findById(id);
+			
+			deletefile(cliente);
+			
 			clienteService.delete(id);			
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar el delete en la Base de Datos");
@@ -139,5 +157,76 @@ public class ClienteRestController {
 		} 
 		response.put("mensaje", "El cliente ha sido eliminado con èxito!");
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+	
+	@PostMapping("/upload")
+	public ResponseEntity<?> upload(@RequestParam("File") MultipartFile file, @RequestParam("id") Long id){
+		Map<String, Object> response = new HashMap<>();
+		
+		Cliente cliente = clienteService.findById(id);
+		
+		if (!file.isEmpty()) {
+			String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename().replace(" ", "");
+			Path ruta = Paths.get("upload").resolve(fileName).toAbsolutePath();
+			
+			log.info(ruta.toString());
+			
+			try {
+				Files.copy(file.getInputStream(), ruta);				
+			} catch (IOException e) {
+				response.put("mensaje", "Error al realizar al subir la imagen");
+				response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			deletefile(cliente);
+			
+			cliente.setFoto(fileName);
+			
+			clienteService.save(cliente);
+			
+			response.put("cliente", cliente);
+			response.put("mensaje", "Has subido correctamente la imagen");
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/uploads/img/{nameFoto:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String nameFoto) {
+		Path ruta = Paths.get("upload").resolve(nameFoto).toAbsolutePath();
+		
+		log.info(ruta.toString());
+		
+		Resource resource = null;
+		
+		try {
+			resource = new UrlResource(ruta.toUri());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		if (!resource.exists() && !resource.isReadable()) {
+			throw new RuntimeException("Error al cargar la image: " + nameFoto);
+		}
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"");
+		
+		return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
+	}
+	
+	public void deletefile(Cliente cliente) {
+		String fotoAnt = cliente.getFoto();
+		
+		if (fotoAnt != null && fotoAnt.length() > 0) {
+			Path rutaAnt = Paths.get("upload").resolve(fotoAnt).toAbsolutePath();
+			
+			File fileAnte = rutaAnt.toFile();
+			
+			if (fileAnte.exists() && fileAnte.canRead()) {
+				fileAnte.delete();
+			}
+		}
 	}
 }
--- api-service-clientes/src/main/java/com/api/app/clientes/model/entity/Cliente.java
+++ api-service-clientes/src/main/java/com/api/app/clientes/model/entity/Cliente.java
@@ -8,12 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+//import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -23,25 +24,41 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
+	
 	@NotEmpty(message = "No puede estar vacio")
 	@Size(min = 4, max = 12, message = "El tamaño debe estar entre 4 y 12 caracteres")
 	@Column(nullable = false)
 	private String nombre;
+	
 	@NotEmpty(message = "No puede estar vacio")
 	private String apellido;
+	
 	@NotEmpty(message = "No puede estar vacio")
 	@Email(message = "El correo debe tener un formato adecuado")
 	@Column(nullable = false, unique = true)
 	private String email;
+	
+	@NotNull(message = "No puede estar vacio")
 	@Column(name = "create_at")
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.DATE)	
 	private Date createAt;
+	
+	private String Foto;
 
+	/* Se Pidede desde el Front
 	@PrePersist
 	public void prePersist() {
 		createAt =  new Date();
-	}
+	}*/
 	
+	public String getFoto() {
+		return Foto;
+	}
+
+	public void setFoto(String foto) {
+		Foto = foto;
+	}
+
 	public Long getId() {
 		return Id;
 	}
